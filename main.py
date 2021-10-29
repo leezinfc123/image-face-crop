7d052de05f93c9f24d8f88d5a6712759a74a947b
Nambaby <57514617+NamDoan221@users.noreply.github.com>
2021-10-29 16:35:52 +0700

    Update main.py
    fix bug CORs

--- main.py
+++ main.py
@@ -5,11 +5,19 @@
 """
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from controller.process_image import crop_face_image
 
 app = FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
